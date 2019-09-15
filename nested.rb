fcd3073d2951bf1e60470db66c9b5972aaf16b7c
Tami Reichold <reichold.tamara@gmail.com>
2019-09-15 15:01:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -35,6 +34,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -53,6 +53,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -71,7 +72,8 @@ def adding_matz
           :languages => ["C"]
         }
      }  
-
+  programmer_hash[:yukihiro_matsumoto]=[]
+  return programmer_hash
 end
 
 def changing_alan
@@ -90,6 +92,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]="GUI"
+  return programmer_hash
 end
 
 def adding_to_dennis
@@ -108,4 +112,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
+  return programmer_hash
 end
